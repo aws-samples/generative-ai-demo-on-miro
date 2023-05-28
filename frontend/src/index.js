3a0b377438feb174df8dadc922b0e62532634aa7
dkkhoros <dkkhoros@amazon.com>
2023-05-28 19:07:31 +0200

    0.3.8: - re-route lambda call to CloudFront -> API Gw - added installation steps - working lambda and two endpoins - image creation and image modification Lambda: use environment variables, update docker base image to public ECR Sagemaker endpoins: used sagemaker notebooks Added use-case 4: Image inpainting

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -26,14 +26,16 @@ async function init() {
   //
   //
   // debug: print application log message and site origin
-
+  let app_origin = location.origin;
+  console.log("Generative AI demo for Miro loading...")
+  console.log("Server origin: ", app_origin)
 
   board.ui.on("icon:click", async () => {
-    let app_origin = location.origin;
-    console.log("Generative AI demo for Miro running...")
-    console.log("Server origin: ", app_origin)
+
     // procedure parameters
-    const apiUrl = "https://4ay7wa1sij.execute-api.us-east-1.amazonaws.com/default/lambda-generative-ai-demo-on-miro"
+    //const apiUrl = "https://4ay7wa1sij.execute-api.us-east-1.amazonaws.com/default/lambda-generative-ai-demo-on-miro"
+    //redirect to CloudFront
+    const apiUrl = app_origin + "/api/lambda-generative-ai-demo-on-miro"
 
 
     // Get selected items and filter images
